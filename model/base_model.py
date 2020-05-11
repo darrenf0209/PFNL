01623ae08ab224625f8e6eff829644fb3f4c2ae1
Darren Flaks <darrenf0209@gmail.com>
2020-05-12 07:21:16 +1000

    Removing unused data pipeline functions

--- model/base_model.py
+++ model/base_model.py
@@ -38,7 +38,6 @@ class VSR(object):
         self.save_dir = './checkpoint'
         self.log_dir = './eval_log.txt'
 
-    
     def null_pipeline(self):
         def read_data():
             # Randomly crops self.data_queue into a 1-D tensor with size between 1 and num_frames
@@ -122,10 +121,6 @@ class VSR(object):
             batch_in, batch_gt = tf.train.batch([input, gt], batch_size=self.batch_size, num_threads=3,
                                                 capacity=self.batch_size * 2)
         return batch_in, batch_gt
-
-
-        
-########################################################################################################################################
         
     def tiled_pipeline(self):
         def prepprocessing(gt=None):
@@ -217,124 +212,6 @@ class VSR(object):
 
         return inp, gt
         #####################################################################################################################
-
-
-    def frvsr_input_producer(self):
-        def read_data():
-            idx0 = self.num_frames // 2
-            data_seq = tf.random_crop(self.data_queue, [2, self.num_frames])
-            input = tf.stack(
-                [tf.image.decode_png(tf.read_file(data_seq[0][i]), channels=3) for i in range(self.num_frames)])
-            # gt = tf.stack([tf.image.decode_png(tf.read_file(data_seq[1][idx0]), channels=3)])
-            gt = tf.stack(
-                [tf.image.decode_png(tf.read_file(data_seq[1][i]), channels=3) for i in range(self.num_frames)])
-            input, gt = prepprocessing(input, gt)
-            print('Input producer shape: ', input.get_shape(), gt.get_shape())
-            return input, gt
-
-        def prepprocessing(input, gt=None):
-            input = tf.cast(input, tf.float32) / 255.0
-            gt = tf.cast(gt, tf.float32) / 255.0
-
-            shape = tf.shape(input)[1:]
-            size = tf.convert_to_tensor([self.in_size, self.in_size, 3], dtype=tf.int32, name="size")
-            check = tf.Assert(tf.reduce_all(shape >= size), ["Need value.shape >= size, got ", shape, size])
-            shape = control_flow_ops.with_dependencies([check], shape)
-
-            limit = shape - size + 1
-            offset = tf.random_uniform(tf.shape(shape), dtype=size.dtype, maxval=size.dtype.max, seed=None) % limit
-
-            offset_in = tf.concat([[0], offset], axis=-1)
-            size_in = tf.concat([[self.num_frames], size], axis=-1)
-            input = tf.slice(input, offset_in, size_in)
-            offset_gt = tf.concat([[0], offset[:2] * self.scale, [0]], axis=-1)
-            size_gt = tf.concat([[self.num_frames], size[:2] * self.scale, [3]], axis=-1)
-            gt = tf.slice(gt, offset_gt, size_gt)
-
-            input.set_shape([self.num_frames, self.in_size, self.in_size, 3])
-            gt.set_shape([self.num_frames, self.in_size * self.scale, self.in_size * self.scale, 3])
-            return input, gt
-
-        pathlist = open(self.train_dir, 'rt').read().splitlines()
-        random.shuffle(pathlist)
-        with tf.variable_scope('input'):
-            inList_all = []
-            gtList_all = []
-            for dataPath in pathlist:
-                inList = sorted(glob.glob(os.path.join(dataPath, 'blur{}/*.png'.format(self.scale))))
-                gtList = sorted(glob.glob(os.path.join(dataPath, 'truth_downsize_2/*.png')))
-                inList_all.append(inList)
-                gtList_all.append(gtList)
-            inList_all = tf.convert_to_tensor(inList_all, dtype=tf.string)
-            gtList_all = tf.convert_to_tensor(gtList_all, dtype=tf.string)
-
-            self.data_queue = tf.train.slice_input_producer([inList_all, gtList_all], capacity=self.batch_size * 2)
-            input, gt = read_data()
-            batch_in, batch_gt = tf.train.batch([input, gt], batch_size=self.batch_size, num_threads=3,
-                                                capacity=self.batch_size * 2)
-        return batch_in, batch_gt
-
-    def double_input_producer(self):
-        def read_data():
-            idx0 = self.num_frames // 2
-            data_seq = tf.random_crop(self.data_queue, [2, self.num_frames])
-            input = tf.stack(
-                [tf.image.decode_png(tf.read_file(data_seq[0][i]), channels=3) for i in range(self.num_frames)])
-            gt = tf.stack([tf.image.decode_png(tf.read_file(data_seq[1][idx0]), channels=3)])
-            input, gt = prepprocessing(input, gt)
-
-            flip = tf.random_uniform((1, 3), minval=0.0, maxval=1.0, dtype=tf.float32, seed=None,
-                                     name=None)  # if training gets worse, comment the data flip part out
-            input = tf.where(flip[0][0] < 0.5, input, input[:, ::-1])
-            input = tf.where(flip[0][1] < 0.5, input, input[:, :, ::-1])
-            input = tf.where(flip[0][2] < 0.5, input, tf.transpose(input, perm=(0, 2, 1, 3)))
-            gt = tf.where(flip[0][0] < 0.5, gt, gt[:, ::-1])
-            gt = tf.where(flip[0][1] < 0.5, gt, gt[:, :, ::-1])
-            gt = tf.where(flip[0][2] < 0.5, gt, tf.transpose(gt, perm=(0, 2, 1, 3)))
-            print('Input producer shape: ', input.get_shape(), gt.get_shape())
-            return input, gt
-
-        def prepprocessing(input, gt=None):
-            input = tf.cast(input, tf.float32) / 255.0
-            gt = tf.cast(gt, tf.float32) / 255.0
-
-            shape = tf.shape(input)[1:]
-            size = tf.convert_to_tensor([self.in_size, self.in_size, 3], dtype=tf.int32, name="size")
-            check = tf.Assert(tf.reduce_all(shape >= size), ["Need value.shape >= size, got ", shape, size])
-            shape = control_flow_ops.with_dependencies([check], shape)
-
-            limit = shape - size + 1
-            offset = tf.random_uniform(tf.shape(shape), dtype=size.dtype, maxval=size.dtype.max, seed=None) % limit
-
-            offset_in = tf.concat([[0], offset], axis=-1)
-            size_in = tf.concat([[self.num_frames], size], axis=-1)
-            input = tf.slice(input, offset_in, size_in)
-            offset_gt = tf.concat([[0], offset[:2] * self.scale, [0]], axis=-1)
-            size_gt = tf.concat([[1], size[:2] * self.scale, [3]], axis=-1)
-            gt = tf.slice(gt, offset_gt, size_gt)
-
-            input.set_shape([self.num_frames, self.in_size, self.in_size, 3])
-            gt.set_shape([1, self.in_size * self.scale, self.in_size * self.scale, 3])
-            return input, gt
-
-        pathlist = open(self.train_dir, 'rt').read().splitlines()
-        random.shuffle(pathlist)
-        with tf.variable_scope('input'):
-            inList_all = []
-            gtList_all = []
-            for dataPath in pathlist:
-                inList = sorted(glob.glob(os.path.join(dataPath, 'blur{}/*.png'.format(self.scale))))
-                gtList = sorted(glob.glob(os.path.join(dataPath, 'truth_downsize_2/*.png')))
-                inList_all.append(inList)
-                gtList_all.append(gtList)
-            inList_all = tf.convert_to_tensor(inList_all, dtype=tf.string)
-            gtList_all = tf.convert_to_tensor(gtList_all, dtype=tf.string)
-
-            self.data_queue = tf.train.slice_input_producer([inList_all, gtList_all], capacity=self.batch_size * 2)
-            input, gt = read_data()
-            batch_in, batch_gt = tf.train.batch([input, gt], batch_size=self.batch_size, num_threads=3,
-                                                capacity=self.batch_size * 2)
-        return batch_in, batch_gt
 
     def single_input_producer(self):
         def read_data():
@@ -471,7 +348,6 @@ class VSR(object):
 
     def testvideos(self):
         pass
-
 
 if __name__ == '__main__':
     model = VSR()

6ebc5500c250a5aec0f14d5c682963b68617dc42
Darren Flaks <darrenf0209@gmail.com>
2020-05-26 14:52:30 +1000

    Preventing testing models with the wrong name

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 from model.control import PFNL_control
-from model.control import NAME as CONTROL_NAME
+from model.control import NAME as CONTROL
 from model.alternative import PFNL_alternative
-from model.alternative import NAME as ALTERNATIVE_NAME
+from model.alternative import NAME as ALTERNATIVE
 from model.null import PFNL_null
-from model.null import NAME as NULL_NAME
+from model.null import NAME as NULL
 import time
 from utilities.pre_processing import resize_imgs_truth
 from utilities.pre_processing import *
@@ -19,17 +19,25 @@ if __name__=='__main__':
     # slice_imgs_truth(path='test\\vid4\\', use='first', num_tiles=4, copy_original=True, name='DELETE_TRIAL')
     # copy_to_loc(path='test\\vid4\\', stop=2, name="DELETE_AGAIN")
 
-    # Choosing the model
+    # Choose model
     # model = PFNL_control()
-    # model = PFNL_alternative()
-    model = PFNL_null()
-    # print('Model loaded!')
+    # model = PFNL_null()
+    model = PFNL_alternative()
+    print('Model loaded!')
 
     # Training the model
-    model.train()
+    # model.train()
     # print('Training finished')
 
     # Testing the model
-    # model.testvideos('test\\udm10\\', name='{}_best_{}'.format(NAME, int(time.time())))
-    # model.testvideos('test\\vid4\\', name='LR_LR_DELETE_{}_{}'.format(NAME, int(time.time())))
-    # print('Finished')
+    if isinstance(model, PFNL_control):
+        model.testvideos('test\\vid4\\', name='{}_{}'.format(CONTROL, time.strftime("%Y%m%d", time.localtime())))
+        model.testvideos('test\\udm10\\', name='{}_{}'.format(CONTROL, time.strftime("%Y%m%d", time.localtime())))
+    elif isinstance(model, PFNL_null):
+        model.testvideos('test\\vid4\\', name='{}_{}'.format(NULL, time.strftime("%Y%m%d", time.localtime())))
+        model.testvideos('test\\udm10\\', name='{}_{}'.format(NULL, time.strftime("%Y%m%d", time.localtime())))
+    else:
+        model.testvideos('test\\vid4\\', name='{}_{}'.format(ALTERNATIVE, time.strftime("%Y%m%d", time.localtime())))
+        model.testvideos('test\\udm10\\', name='{}_{}'.format(ALTERNATIVE, time.strftime("%Y%m%d", time.localtime())))
+
+    print('Runtime finished')

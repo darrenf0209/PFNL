0080f8f4f99d8909e437fdaaf93d219ff888deff
Darren Flaks <darrenf0209@gmail.com>
2020-11-05 19:04:13 +1100

    Generate gif script

---
+++ utilities/generate_gif.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import glob
+
+'''
+This is a simple script to generate a gif by passing in a path of frames in order. 
+Pass in a path to rertieve the images and save the file with approrpiate name.
+'''
+# Create the frames
+frames = []
+imgs = glob.glob("test\\vid4\\calendar\\Proposed_Mean_Loss_20200817_Proposed_Loss_20200819\\*.png")
+for i in imgs:
+    print(i)
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+
+# Save into a GIF file that loops forever
+frames[0].save('report/calendar_mean.gif', format='GIF',
+               append_images=frames[5:-5],
+               save_all=True,
+               duration=300, loop=0)
